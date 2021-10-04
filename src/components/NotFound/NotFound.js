bb3ae617e41e9b12083aea53316f2c2741ec6518
Arifulislamt <arifulislamt06@gmail.com>
2021-10-04 19:42:47 +0600

    final commit

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import img from '../../images/404 Error with a cute animal-pana.png';
+import Header from '../Header/Header';
 
 const NotFound = () => {
     return (
-        <div className="footer-costom p-5 mt-5 mb-5">
-        <div className="costom-fimg d-flex justify-content-center">
-            <img className="" src={img} alt="" />
-        </div>
-        <div className="d-flex justify-content-center mt-2 costom-hobar">
-            <h1>Oops! Page Not Found</h1>
-        </div>
+        <div>
+            <Header></Header>
+            <div className="footer-costom p-5 mt-5 mb-5">
+                <div className="costom-fimg d-flex justify-content-center">
+                    <img className="" src={img} alt="" />
+                </div>
+                <div className="d-flex justify-content-center mt-2 costom-hobar">
+                    <h1>Oops! Page Not Found</h1>
+                </div>
+            </div>
         </div>
     );
 };
